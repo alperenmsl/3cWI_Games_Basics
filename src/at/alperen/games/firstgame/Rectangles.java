daa01e0a67712dd5dcf750373db264e92e44a983
Alperen <alperen@example.com>
2025-02-26 09:04:09 +0100

    Aufgabe 01 Das erste Spiel Finished

--- src/at/alperen/games/firstgame/Rectangles.java
+++ src/at/alperen/games/firstgame/Rectangles.java
@@ -1,23 +1,51 @@
 package at.alperen.games.firstgame;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.*;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tests.AnimationTest;
+
+import java.awt.*;
 
 public class Rectangles extends BasicGame {
+
+    private int x;
+    private int y;
+
+
+    public Rectangles(String title) {
+        super(title);
+    }
+
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-
+        this.x = 100;
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
+        this.x++;
+        this.y++;
+        if (this.x > 800){
+            this.x = 0;
+            this.y = 0;
+        }
 
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-
+        graphics.drawRect(this.x, this.y, 100, 100);
+        graphics.drawString("Hello Player!", 50, 50);
     }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
