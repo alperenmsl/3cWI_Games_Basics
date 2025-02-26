9d8f8ac89307d5ac50f5b53d2f1d989ff0a4c78e
Alperen <alperen@example.com>
2025-02-26 08:34:35 +0100

    Started OO Games - 01 Das erste Spiel

---
+++ src/at/alperen/games/firstgame/Rectangles.java
@@ -0,0 +1,23 @@
+package at.alperen.games.firstgame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Rectangles extends BasicGame {
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
