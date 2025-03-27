810576a3b3f7c1771e8e86d26584ff547698a4ef
Alperen <alperen@example.com>
2025-03-27 08:56:27 +0100

    Aufgabe 08 Snowworld Game finished

---
+++ src/at/alperen/games/snowworld/Actor.java
@@ -0,0 +1,11 @@
+package at.alperen.games.snowworld;
+
+import org.newdawn.slick.Graphics;
+
+public interface Actor {
+    public void render(Graphics graphics);
+
+    public void update(int delta);
+
+
+}
